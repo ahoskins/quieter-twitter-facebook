9158cffef84eed3723206342de434a7405f6de0c
Andrew Hoskins <andrew.hoskins16@gmail.com>
2017-10-08 17:27:26 -0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Quieter-Twitter-Facebook
+
+Focus only on what you came for. This is a chrome extension for hiding distracting content on two of our favourite social networks.
---
+++ content.js
@@ -0,0 +1,6 @@
+var style = document.createElement("link");
+style.rel = "stylesheet";
+style.type = "text/css";
+style.href =
+  "https://rawgit.com/ahoskins/quieter-twitter-facebook/master/styles.css";
+document.body.appendChild(style);
